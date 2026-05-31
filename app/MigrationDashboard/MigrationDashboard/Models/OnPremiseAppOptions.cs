3c307e9208cf5d0746fc2c9fadce1d80538eb30b
Phan Hoang Ke <kedeptry2004@gmail.com>
2026-05-31 12:26:23 +0700

    Ignore local private appsettings

---
+++ app/MigrationDashboard/MigrationDashboard/Models/OnPremiseAppOptions.cs
@@ -0,0 +1,8 @@
+namespace MigrationDashboard.Models;
+
+public class OnPremiseAppOptions
+{
+    public string SourcePath { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
